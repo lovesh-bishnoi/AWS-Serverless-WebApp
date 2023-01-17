81782d8454404bad2e1cdf1b54096ea92de3ce63
Lovesh Bishnoi <64860326+Luv7k@users.noreply.github.com>
2023-01-17 17:12:32 +0100

    Lambda_function
    At the initial stage

--- lambda.py
+++ lambda.py
@@ -1,6 +1,7 @@
 # import the JSON utility package
 import json
 
+"""
 # import the AWS SDK (for Python the package name is boto3)
 import boto3
 # import two packages to help us with dates and date formatting
@@ -9,22 +10,22 @@ from time import gmtime, strftime
 # create a DynamoDB object using the AWS SDK
 dynamodb = boto3.resource('dynamodb')
 # use the DynamoDB object to select our table
-table = dynamodb.Table('AngleofHandsDB')
+table = dynamodb.Table('ClockAngles-DynamoDB')
 # store the current time in a human readable format in a variable
 now = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
-
+"""
 
 # define the handler function that the Lambda service will use an entry point
 def lambda_handler(event, context):
     # extract the two numbers from the Lambda service's event object
     hours = int(event.get('hours'))
     while hours > 23:
-        print("The number of hours should be between 0 and 23")
         hours = int(event.get('hours'))
     mins = int(event.get('mins'))
     while mins > 59:
-        print("The number of minutes should be between 0 and 59")
+
         mins = int(event.get('mins'))
+
     if hours > 12:
         hours = hours - 12
     angle_mins = mins * 6
@@ -33,12 +34,14 @@ def lambda_handler(event, context):
     if angle_btw_hands > 180:
         angle_btw_hands = 360 - angle_btw_hands
 
+    """
     # write result and time to the DynamoDB table using the object we instantiated and save response in a variable
     response = table.put_item(
         Item={
             'ID': str(angle_btw_hands),
             'LatestGreetingTime': now
         })
+    """
 
     # return a properly formatted JSON object
     return {
